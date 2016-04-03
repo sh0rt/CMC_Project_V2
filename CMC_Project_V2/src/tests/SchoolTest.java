b37fe8dd517aaa485566577522aa3a0cfdf3f903
Megan Pekarek <mrpekarek@csbsju.edu>
2016-04-03 14:47:04 -0500

    updated school test

--- CMC_Project_V2/src/tests/SchoolTest.java
+++ CMC_Project_V2/src/tests/SchoolTest.java
@@ -10,144 +10,162 @@ public class SchoolTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		school1 = new School("University of Minnesota","Minnesota", "URBAN", "UNKNOWN", 500000, .45, 20,30,5000.0,.3,1000,.5,.3,4,3,5);
+		school1 = new School("University of Minnesota","Minnesota", "URBAN", "UNKNOWN", 500000, .45, 20,30,5000.0,.3,1000,.5,.3,4,3,5,"compsci");
 	}
 	//-----------------------------------------------------------Test getters-------------------------------------------------------------
 	@Test
 	public void getState() {
-		assertTrue("getState not working", school1.getState().equals("Minnesota"));
+		assertTrue("getState working", school1.getState().equals("Minnesota"));
 	}
 	@Test
 	public void getLocation() {
-		assertTrue("getLocation not working", school1.getLocation().equals("URBAN"));
+		assertTrue("getLocation working", school1.getLocation().equals("URBAN"));
 	}
 	@Test
 	public void getControl() {
-		assertTrue("getControl not working", school1.getControl().equals("UNKNOWN"));
+		assertTrue("getControl working", school1.getControl().equals("UNKNOWN"));
 	}
 	@Test
 	public void getNumberOfStudents() {
-		assertTrue("getNumberOfStudents not working", school1.getNumStudents() == 500000);
+		assertTrue("getNumberOfStudents working", school1.getNumStudents() == 500000);
 	}
 	@Test
 	public void getPercentFemales() {
-		assertTrue("getPercentFemales not working", school1.getPercentFemale() == .45);
+		assertTrue("getPercentFemales working", school1.getPercentFemale() == .45);
 	}
 	@Test
 	public void getSATVerbal() {
-		assertTrue("getSatVerbal not working", school1.getSatVerbal() == 20);
+		assertTrue("getSatVerbal working", school1.getSatVerbal() == 20);
 	}
 	@Test
 	public void getSatMath() {
-		assertTrue("getSatMath not working", school1.getSatMath() == 30);
+		assertTrue("getSatMath working", school1.getSatMath() == 30);
 	}
 	@Test
 	public void getExpenses() {
-		assertTrue("getSatMath not working", school1.getExpenses() == 5000.0);
+		assertTrue("getSatMath working", school1.getExpenses() == 5000.0);
 	}
 	@Test
 	public void getPercentFinancialAid() {
-		assertTrue("getPercentFinancialAid not working", school1.getPercentFinAid() == .3);
+		assertTrue("getPercentFinancialAid working", school1.getPercentFinAid() == .3);
 	}
 	@Test
 	public void getNumberOfAplicants() {
-		assertTrue("getNumberOfApplicants not working", school1.getNumApplicants() == 1000);
+		assertTrue("getNumberOfApplicants working", school1.getNumApplicants() == 1000);
 	}
 	@Test
 	public void getPercentAdmitted() {
-		assertTrue("getPercentAdmitted not working", school1.getPercentAdmitted() == .5);
+		assertTrue("getPercentAdmitted working", school1.getPercentAdmitted() == .5);
 	}
 	@Test
 	public void getPercentEnrolled() {
-		assertTrue("getPercentFinancialAid not working", school1.getPercentEnrolled() == .3);
+		assertTrue("getPercentFinancialAid working", school1.getPercentEnrolled() == .3);
 	}
 	@Test
 	public void getAcademicScale() {
-		assertTrue("getAcademicScale not working", school1.getAcademicScale() == 4);
+		assertTrue("getAcademicScale working", school1.getAcademicScale() == 4);
 	}
 	@Test
 	public void getSocialScale() {
-		assertTrue("getSocialScale not working", school1.getSocialscale() == 3);
+		assertTrue("getSocialScale working", school1.getSocialscale() == 3);
 	}
 	@Test
 	public void getQualOfLifeScale() {
-		assertTrue("getQualOfLifeScale not working", school1.getQualOfLife() == 5);
+		assertTrue("getQualOfLifeScale working", school1.getQualOfLife() == 5);
+	}
+	@Test
+	public void testGetEmphasis() {
+		assertTrue("getEmphasis working", school1.getQualOfLife() == "compsci");
+	}
+	@Test
+	public void testGetSchool() {
+		assertTrue("getSchool working", school1.getQualOfLife() == "University of Minnesota");
 	}
 	//----------------------------------------------------------test setters------------------------------- ADD IN SETTERS TO ONES BELOW!!!!
 	@Test
 	public void setState() {
 		school1.setState("test");
-		assertTrue("setState not working", school1.getState().equals("test"));
+		assertTrue("setState working", school1.getState().equals("test"));
 	}
 	@Test
 	public void setLocation() {
 		school1.setLocation("test");
-		assertTrue("setLocation not working", school1.getLocation().equals("test"));
+		assertTrue("setLocation working", school1.getLocation().equals("test"));
 	}
 	@Test
 	public void setControl() {
 		school1.setControl("test");
-		assertTrue("setControl not working", school1.getControl().equals("test"));
+		assertTrue("setControl working", school1.getControl().equals("test"));
 	}
 	@Test
 	public void setNumberOfStudents() {
 		school1.setNumStudents(1000);
-		assertTrue("setNumberOfStudents not working", school1.getNumStudents() == 1000);
+		assertTrue("setNumberOfStudents working", school1.getNumStudents() == 1000);
 	}
 	@Test
 	public void setPercentFemales() {
 		school1.setPercentFemale(20.0);
-		assertTrue("setPercentFemales not working", school1.getPercentFemale() == 20.0);
+		assertTrue("setPercentFemales working", school1.getPercentFemale() == 20.0);
 	}
 	@Test
 	public void setSATVerbal() {
 		school1.setSatVerbal(300.0);
-		assertTrue("setSatVerbal not working", school1.getSatVerbal() == 300.0);
+		assertTrue("setSatVerbal working", school1.getSatVerbal() == 300.0);
 	}
 	@Test
 	public void setSatMath() {
 		school1.setSatMath(300.0);
-		assertTrue("setSatMath not working", school1.getSatMath() == 300.0);
+		assertTrue("setSatMath working", school1.getSatMath() == 300.0);
 	}
 	@Test
 	public void setExpenses() {
 		school1.setExpenses(1000.0);
-		assertTrue("setSatMath not working", school1.getExpenses() == 1000.0);
+		assertTrue("setSatMath working", school1.getExpenses() == 1000.0);
 	}
 	@Test
 	public void setPercentFinancialAid() {
 		school1.setPercentFinAid(10.0);
-		assertTrue("setPercentFinancialAid not working", school1.getPercentFinAid() == 10.0);
+		assertTrue("setPercentFinancialAid working", school1.getPercentFinAid() == 10.0);
 	}
 	@Test
 	public void setNumberOfAplicants() {
 		school1.setNumApplicants(1);
-		assertTrue("setNumberOfApplicants not working", school1.getNumApplicants() == 1);
+		assertTrue("setNumberOfApplicants working", school1.getNumApplicants() == 1);
 	}
 	@Test
 	public void setPercentAdmitted() {
 		school1.setPercentAdmitted(.3);
-		assertTrue("setPercentAdmitted not working", school1.getPercentAdmitted() == .3);
+		assertTrue("setPercentAdmitted working", school1.getPercentAdmitted() == .3);
 	}
 	@Test
 	public void setPercentEnrolled() {
 		school1.setPercentEnrolled(.4);
-		assertTrue("setPercentFinancialAid not working", school1.getPercentEnrolled() == .4);
+		assertTrue("setPercentFinancialAid working", school1.getPercentEnrolled() == .4);
 	}
 	@Test
 	public void setAcademicScale() {
 		school1.setAcademicScale(2);
-		assertTrue("setAcademicScale not working", school1.getAcademicScale() == 2);
+		assertTrue("setAcademicScale working", school1.getAcademicScale() == 2);
 	}
 	@Test
 	public void setSocialScale() {
 		school1.setSocialscale(2);
-		assertTrue("setSocialScale not working", school1.getSocialscale() == 2);
+		assertTrue("setSocialScale working", school1.getSocialscale() == 2);
 	}
 	@Test
 	public void setQualOfLifeScale() {
 		school1.setQualOfLife(1);
-		assertTrue("setQualOfLifeScale not working", school1.getQualOfLife() == 1);
+		assertTrue("setQualOfLifeScale working", school1.getQualOfLife() == 1);
+	}
+	@Test
+	public void testSetEmphasis() {
+		school1.setEmphasis("code");
+		assertTrue("setEmphasis working", school1.getQualOfLife() == "code");
+	}
+	@Test
+	public void testSetSchool() {
+		school1.setSchool("U of M");
+		assertTrue("setSchool working", school1.getQualOfLife() == "U of M");
 	}
 	//---------------------------------------------Test Failures------------------------------------------------
 	@Test(expected = IllegalArgumentException.class)
@@ -249,5 +267,13 @@ public class SchoolTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void setOverQualOfLife(){
 		school1.setQualOfLife(6);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void setEmptyEmphasis() {
+		school1.setEmphasis("");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void setEmptySchoolName() {
+		school1.setSchool("");
 	}
 }
